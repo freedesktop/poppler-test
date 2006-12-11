87e739aa4aa4e196a7580293ca321222404f915c
Jeff Muizelaar <jeff@infidigm.net>
2006-12-11 13:05:22 -0500

    Add support for comparing the text extracted from a pdf page.
    Text comparison is invoked with the '-t' flag. Also, add the appropriate
    reference text for the text.pdf test.

--- test-poppler.c
+++ test-poppler.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <poppler.h>
 #include <glob.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "util.h"
 #include "read-cache.h"
 #define FAIL(msg) \
 	do { fprintf (stderr, "FAIL: %s\n", msg); exit (-1); } while (0)
@@ -11,9 +17,13 @@
 
 #define POPPLER_TEST_LOG_SUFFIX ".log"
 #define POPPLER_TEST_PNG_SUFFIX "-out.png"
-#define POPPLER_TEST_REF_SUFFIX "-ref.png"
-#define POPPLER_TEST_DIFF_SUFFIX "-diff.png"
+#define POPPLER_TEST_REF_PNG_SUFFIX "-ref.png"
+#define POPPLER_TEST_DIFF_PNG_SUFFIX "-diff.png"
+#define POPPLER_TEST_TXT_SUFFIX "-out.txt"
+#define POPPLER_TEST_REF_TXT_SUFFIX "-ref.txt"
+#define POPPLER_TEST_DIFF_TXT_SUFFIX "-diff.txt"
 
+bool text_output = false;
 
 typedef enum poppler_test_status {
       POPPLER_TEST_SUCCESS = 0,
@@ -43,8 +53,8 @@ poppler_test_status_t gdk_pixbuf_compare(GdkPixbuf *pixbuf, char *page_name)
   if (!srcdir)
     srcdir = ".";
   asprintf (&png_name, "%s-%s%s", page_name, backend, POPPLER_TEST_PNG_SUFFIX);
-  asprintf (&ref_name, "%s/%s-%s%s", srcdir, page_name, backend, POPPLER_TEST_REF_SUFFIX);
-  asprintf (&diff_name, "%s-%s%s", page_name, backend, POPPLER_TEST_DIFF_SUFFIX);
+  asprintf (&ref_name, "%s/%s-%s%s", srcdir, page_name, backend, POPPLER_TEST_REF_PNG_SUFFIX);
+  asprintf (&diff_name, "%s-%s%s", page_name, backend, POPPLER_TEST_DIFF_PNG_SUFFIX);
 #ifdef PNG_SAVE
   gdk_pixbuf_save (pixbuf, png_name, "png", &error, NULL);
   pixels_changed = image_diff (png_name, ref_name, diff_name);
@@ -79,6 +89,51 @@ poppler_test_status_t gdk_pixbuf_compare(GdkPixbuf *pixbuf, char *page_name)
   return pixels_changed ? POPPLER_TEST_FAILURE : POPPLER_TEST_SUCCESS;
 }
 
+poppler_test_status_t poppler_test_page_text(PopplerPage *page, char *text_name)
+{
+  double width, height;
+  char *txt_name, *ref_name, *diff_name;
+  char *srcdir;
+  poppler_test_status_t ret = POPPLER_TEST_SUCCESS;
+  PopplerRectangle *rect = poppler_rectangle_new();
+  poppler_page_get_size (page, &width, &height);
+
+  /* Get the strings ready that we'll need. */
+  srcdir = getenv ("srcdir");
+  if (!srcdir)
+    srcdir = ".";
+
+  asprintf (&txt_name, "%s%s", text_name, POPPLER_TEST_TXT_SUFFIX);
+  asprintf (&ref_name, "%s/%s%s", srcdir, text_name, POPPLER_TEST_REF_TXT_SUFFIX);
+  asprintf (&diff_name, "%s%s", text_name, POPPLER_TEST_DIFF_TXT_SUFFIX);
+  rect->x1 = rect->y1 = 0.0;
+  rect->x2 = width;
+  rect->y2 = height;
+  char *text = poppler_page_get_text(page, rect);
+  int len = strlen(text);
+  if (len) {
+    FILE *file = fopen(txt_name, "wb");
+    char *diff_cmd;
+    fwrite(text, 1, len, file);
+    fclose(file);
+    asprintf (&diff_cmd, "diff -u %s %s > %s", txt_name, ref_name, diff_name);
+    int diff_ret = WEXITSTATUS(system(diff_cmd));
+    if (diff_ret == 0 || diff_ret == 2 /* file not found */) {
+      xunlink (diff_name);
+    }
+    if (diff_ret)
+      ret = POPPLER_TEST_FAILURE;
+    free(diff_cmd);
+  }
+  g_free(text);
+  poppler_rectangle_free(rect);
+
+  free(txt_name);
+  free(ref_name);
+  free(diff_name);
+  return ret;
+}
+
 poppler_test_status_t poppler_test_page(char *pdf_file, PopplerDocument *document, int page_index, int n_pages) {
   GdkPixbuf *pixbuf, *thumb;
   double width, height;
@@ -87,10 +142,12 @@ poppler_test_status_t poppler_test_page(char *pdf_file, PopplerDocument *documen
   char *page_format;
   char *page_name;
   char *thumb_name;
+  char *text_name;
 
   asprintf(&page_format, "%%s-%%0%dd", ilog10(n_pages)+1);
   asprintf(&page_name, page_format, pdf_file, page_index);
   asprintf(&thumb_name, "%s-thumb", page_name);
+  asprintf(&text_name, "%s-text", page_name);
 
   page = poppler_document_get_page (document, page_index);
   if (!page)
@@ -103,7 +160,10 @@ poppler_test_status_t poppler_test_page(char *pdf_file, PopplerDocument *documen
   poppler_page_render_to_pixbuf (page, 0, 0, width, height, 1.0, 0, pixbuf);
 
   ret = gdk_pixbuf_compare(pixbuf, page_name);
-  
+
+  if (text_output)
+    ret |= poppler_test_page_text(page, text_name);
+
   thumb = poppler_page_get_thumbnail(page);
   if (thumb)
     ret |= gdk_pixbuf_compare(thumb, thumb_name);
@@ -115,6 +175,8 @@ poppler_test_status_t poppler_test_page(char *pdf_file, PopplerDocument *documen
 
   free(page_format);
   free(page_name);
+  free(thumb_name);
+  free(text_name);
   return ret;
 }
 
@@ -160,10 +222,16 @@ int main(int argc, char **argv)
   int total_secs;
   g_type_init ();
 
+  switch (getopt(argc, argv, "t")) {
+    case 't':
+      text_output = true;
+      break;
+  }
+  printf("%d %d\n", argc, optind);
   gettimeofday(&start, NULL);
-  if (argc > 1) {
-	  cache_init(argv[1]);
-	  for (i=1; i<argc; i++) {
+  if ((argc - optind) >= 1) {
+	  cache_init(argv[optind]);
+	  for (i=optind; i<argc; i++) {
 		  poppler_test(argv[i]);
 	  }
   } else {
